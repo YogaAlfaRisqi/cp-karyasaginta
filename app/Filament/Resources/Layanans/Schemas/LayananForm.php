f03e5a9a0a83dc6a2b07a080a064ed517980bcd6
YogaAlfaRisqi <yogariysqi4@gmail.com>
2025-10-02 11:04:26 +0700

    feat:add Layanan & Article Resource

---
+++ app/Filament/Resources/Layanans/Schemas/LayananForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Layanans\Schemas;
+
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class LayananForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('title')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                FileUpload::make('image')
+                    ->image(),
+                TextInput::make('created_by')
+                    ->numeric(),
+                TextInput::make('updated_by')
+                    ->numeric(),
+            ]);
+    }
+}
