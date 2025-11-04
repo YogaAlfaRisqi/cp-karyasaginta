3102a118b28de9356b4adc42a8cee7aad07822a7
YogaAlfaRisqi <yogariysqi4@gmail.com>
2025-11-04 21:22:49 +0700

    update controller

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -11,19 +11,11 @@ class ProdukController extends Controller
   public function index(Request $request)
   {
 
-    // $produk = [
-    // ['img' => 'Alpukat.jpeg', 'title' => 'Bibit Alpukat Mentega', 'desc' => 'Cocok untuk dataran rendah dan tinggi.'],
-    // ['img' => 'Aren.jpeg', 'title' => 'Durian Musang King', 'desc' => 'Rasa manis legit dan produktivitas tinggi.'],
-    // ['img' => 'Mangga.jpeg', 'title' => 'Mangga Harum Manis', 'desc' => 'Bibit cepat tumbuh dan berbuah besar.'],
-    // ['img' => 'Duku.jpg', 'title' => 'Bibit Duku Super', 'desc' => 'Manis dan cocok di iklim tropis.'],
-    // ['img' => 'Beringin.jpeg', 'title' => 'Beringin', 'desc' => 'Cocok untuk kebutuhan pasar dan dapur.'],
-    // ['img' => 'Durian.jpeg', 'title' => 'Bibit Durian', 'desc' => 'Tahan penyakit dan hasil berkualitas.'],
-    // ];
-
     $filters = [
       'kategori' => $request->get('kategori'),
       'search'   => $request->get('search'),
     ];
+
     $product = Product::with('category')
       ->orderBy('created_at', 'desc')
       ->filter($filters)
@@ -32,6 +24,7 @@ class ProdukController extends Controller
     $categories = Category::all();
 
     $wa = '628126527016';
+    
     return view('pages.produk.produk', compact('product', 'categories', 'wa', 'filters'));
 
   }
