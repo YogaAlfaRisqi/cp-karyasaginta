45c01847a2519402c8e78cf679d3d5d38d72c50b
Moch Rizal Herdiansyah <mochrizalherdiansyah0211@gmail.com>
2025-11-13 15:14:33 +0700

    udpate Kamis 13 Nov

--- app/Filament/Resources/Layanans/Schemas/LayananForm.php
+++ app/Filament/Resources/Layanans/Schemas/LayananForm.php
@@ -19,19 +19,6 @@ class LayananForm
                     ->columnSpanFull(),
                 FileUpload::make('image')
                     ->image()
-<<<<<<< HEAD
-                    ->directory('layanan')
-                    ->disk('public')
-                    ->visibility('public'),
-                TextInput::make('created_by')
-                    ->numeric(),
-                TextInput::make('updated_by')
-                    ->numeric(),
-=======
-                    ->directory('layanans')
-                    ->disk('public')
-                    ->visibility('public'),
->>>>>>> 93877ef09fd6813c2ef78e57fac8426f496737ff
             ]);
     }
 }
--- app/Http/Controllers/LayananController.php
+++ app/Http/Controllers/LayananController.php
@@ -5,34 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\Layanan;
 use Illuminate\Http\Request;
 
-<<<<<<< HEAD
+
 class LayananController extends Controller
 {
-
-
-    public function index(Request $request)
+    public function index(Request $requess)
     {
-        $layanan = Layanan::query()
-            ->latest()
-            ->paginate(10)
-            ->withQueryString();
-
-
-        return view('pages.layanan.layanan', compact('layanan'));
-=======
-class LayananController extends Controller{
-    public function index(Request $requess){
 
         $search = $requess->query('search');
         $layanan = Layanan::query()
             ->when($search, function ($query, $search) {
                 $query->where('nama', 'like', "%{$search}%")
-                      ->orWhere('deskripsi', 'like', "%{$search}%");
+                    ->orWhere('deskripsi', 'like', "%{$search}%");
             })
             ->latest()
-            ->paginate(10) 
-            ->withQueryString(); 
-        return view('pages.layanan.index',compact('layanan', 'search'));
->>>>>>> 93877ef09fd6813c2ef78e57fac8426f496737ff
+            ->paginate(10)
+            ->withQueryString();
+        return view('pages.layanan.index', compact('layanan', 'search'));
     }
 }
